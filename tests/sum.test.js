8a6acbd04e78cdd8d3f0fab8dd975261653bbf50
MateusGustavo22 <githubmateus@gmail.com>
2024-02-20 00:46:26 -0300

    chore: add commitlint

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -2,6 +2,6 @@ import { sum } from '../src/sum'
 
 describe('sum function', () => {
   it('should return 2 + 2 equals 4', () => {
-    expect(sum(2, 4)).toBe(4)
+    expect(sum(2, 2)).toBe(4)
   })
 })
